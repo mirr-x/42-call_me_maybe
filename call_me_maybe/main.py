2bbeb2c6b853996c6abc2b10d09595873225a9f4
mirr <121735620+mirr-x@users.noreply.github.com>
2026-06-11 10:44:42 +0000

    refactor: replace print statements with logging for better traceability

--- call_me_maybe/main.py
+++ call_me_maybe/main.py
@@ -1,9 +1,20 @@
 """ Call Me Maybe Entry Point """
 
+import logging
+
 def main() -> None:
     """ Main function for Call Me Maybe """
-    print("Call Me Maybe started")
+    logging.info("Call Me Maybe started\n")
+    logging.info("Program has Ended")
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        logging.error("Program has been interrupted by the user: %s", e)

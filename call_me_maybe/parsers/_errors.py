87dcd57ca7a1da07306aadc8acedc89fe2ce2ffa
mirr <121735620+mirr-x@users.noreply.github.com>
2026-06-12 10:01:51 +0000

    refactor: enhance error classes in _errors.py with improved formatting and spacing

--- call_me_maybe/parsers/_errors.py
+++ call_me_maybe/parsers/_errors.py
@@ -1,7 +1,34 @@
 """Internal error definitions for the parser."""
 
+
 class ParserError(Exception):
     """Custom parser error to inherit from Exception"""
 
+
 class ParserFileNotFoundError(ParserError):
     """ParserFileNotFoundError inherit from ParserError: if file not found"""
+
+
+class ParserPermissionError(ParserError):
+    """ParserPermissionError inherit from ParserError: if user has no
+    perrmision"""
+
+
+class ParserJSONDecodeError(ParserError):
+    """ParserJSONDecodeError inherit from ParserError: if the file is not in
+    json format"""
+
+
+class ParserInvalidformat(ParserError):
+    """ParserInvalidformat inherit from ParserError: if format invalid
+    inside the file"""
+
+
+class ParserInvalidValue(ParserError):
+    """ParserInvalidValue inherit from ParserError: if value is invalid
+    inside the file"""
+
+
+class ParserValidationError(ParserError):
+    """ParserValidationError inherit from ParserError: if validation failed
+    in pydantic"""

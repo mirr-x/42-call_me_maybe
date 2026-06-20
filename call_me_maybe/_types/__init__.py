5067788165a89bd15efb70c873fa131d8b77e5f3
mirr-x <amir.exe@icloud.com>
2026-06-20 10:37:12 +0000

    feat: add internal initialization for _types and decoding modules

---
+++ call_me_maybe/_types/__init__.py
@@ -0,0 +1,3 @@
+# call_me_maybe/_types/__init__.py
+
+""" Internal initialization for the call_me_maybe package. """
---
+++ call_me_maybe/decoding/__init__.py
@@ -0,0 +1,3 @@
+# call_me_maybe/decoding/__init__.py
+
+""" Internal initialization for the call_me_maybe package. """

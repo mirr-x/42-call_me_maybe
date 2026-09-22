e2a5ab377eb67423247020c3cdc41e0c4a9e8502
mirr-x <amir.exe@icloud.com>
2026-09-22 11:25:52 -0400

    fix: add execution time measurement to main entry point

--- src/__main__.py
+++ src/__main__.py
@@ -4,4 +4,9 @@ from call_me_maybe.__main__ import main
 
 
 if __name__ == "__main__":
+    import time
+    start_time = time.time()
     main()
+    end_time = time.time()
+    print(f"Execution time: {(end_time - start_time) / 60:.2f} minutes")
+
